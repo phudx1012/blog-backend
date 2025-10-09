c93f22f83b287eeb076f9367794d6c1230170597
phudx1012 <phudo101202@gmail.com>
2025-10-09 14:20:27 +0700

    add slug

--- src/main/java/com/blogmanagement/repositories/PostRepository.java
+++ src/main/java/com/blogmanagement/repositories/PostRepository.java
@@ -4,7 +4,6 @@ import com.blogmanagement.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.lang.ScopedValue;
 import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Integer> , JpaSpecificationExecutor<Post> {
